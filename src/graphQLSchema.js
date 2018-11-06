72756e242264118c51425788d133ea5713214d82
Juan Pablo Ovalle Sánchez <jpovalles@unal.edu.co>
2018-11-05 21:03:12 -0500

    Add courses microservice

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -9,20 +9,29 @@ import {
 	studentsQueries,
 	studentsTypeDef
 } from './students/typeDefs';
+import {
+	coursesMutations,
+	coursesQueries,
+	coursesTypeDef
+} from './courses/typeDefs'
 
 import studentsResolvers from './students/resolvers';
+import coursesResolvers from './courses/resolvers'
 
 // merge the typeDefs
 const mergedTypeDefs = mergeSchemas(
 	[
 		'scalar JSON',
-		studentsTypeDef
+		studentsTypeDef,
+		coursesTypeDef
 	],
 	[
-		studentsQueries
+		studentsQueries,
+		coursesQueries
 	],
 	[
-		studentsMutations
+		studentsMutations,
+		coursesMutations
 	]
 );
 
@@ -31,6 +40,7 @@ export default makeExecutableSchema({
 	typeDefs: mergedTypeDefs,
 	resolvers: merge(
 		{ JSON: GraphQLJSON }, // allows scalar JSON
-		studentsResolvers
+		studentsResolvers,
+		coursesResolvers
 	)
 });
